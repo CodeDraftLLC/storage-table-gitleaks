91a8743fb6d4b7ecce79755a1f5d218f53eb494c
thecodedrafter <erik.chavez@codedraft.io>
2025-05-09 16:44:00 -0400

    make it leaky

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 // WIP: Azure Table Storage access (no config yet)
 console.log("This service will eventually list data from Azure Table Storage.");
+const { TableClient } = require("@azure/data-tables");
+
+// ❌ Temporary testing credentials (do not commit!)
+const connectionString = "DefaultEndpointsProtocol=https;AccountName=leakyaccount;AccountKey=superSecret123=;EndpointSuffix=core.windows.net";
+const tableName = "Users";
+
+const client = TableClient.fromConnectionString(connectionString, tableName);
+
+async function listEntities() {
+    const entities = client.listEntities();
+    for await (const entity of entities) {
+        console.log(`Entity: ${JSON.stringify(entity)}`);
+    }
+}
+
+listEntities().catch(console.error);
